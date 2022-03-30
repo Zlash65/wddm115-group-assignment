7160fa884448d2726015100aeead373ec2af5341
aayushi1852 <98124989+aayushi1852@users.noreply.github.com>
2022-03-31 05:13:06 +0530

    added eventlistener

--- js/main.js
+++ js/main.js
@@ -2,3 +2,8 @@ const form = document.querySelector("form");
 const username = document.getElementsById("username");
 const password = document.getElementsById("password");
 const password2 = document.getElementById("password2");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    checkInputs();
+});
