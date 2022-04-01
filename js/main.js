955d83d23c14547efd5a3b228dce4e2ac52f055f
Zlash65 <zarrar65@gmail.com>
2022-03-31 23:46:12 -0400

    fix indentation and formatting for js file

--- js/main.js
+++ js/main.js
@@ -7,32 +7,33 @@ form.addEventListener("submit", (e) => {
     e.preventDefault();
     checkInputs();
 });
+
 function checkInputs() {
-    const usernamevalue = username.value.trim();
-    const passwordvalue = password.value.trim();
-    const password2value = password2.value.trim();
+  const usernamevalue = username.value.trim();
+  const passwordvalue = password.value.trim();
+  const password2value = password2.value.trim();
 
+  // Username inputs
+  if (usernamevalue === "") {
+      setError(username, "Error: username cannot be Empty");
+  } else{
+      setSuccess(username);
+  }
 
-// Username inputs
-    if (usernamevalue ==="") {
-        setError(username, "Error: username cannot be Empty");
-    } else{
-        setSuccess(username);
-    }
+  // Password Input
+  if (passwordvalue === ""){
+      setError(password, "Error: password cannot be empty")
+  }
 
-    // Password Input
-    if (passwordvalue ===""){
-        setError(password, "Error: password cannot be empty")
-    }
-
-    // Check for password conformation input
-    if (password2value ==""){
-        setError(password2, "Error: password cannot be empty") ;
-    } else {
-        setSuccess(password2);
-    }
+  // Check for password conformation input
+  if (password2value === ""){
+      setError(password2, "Error: password cannot be empty") ;
+  } else {
+      setSuccess(password2);
+  }
 }
+
 function setError(input, message){
-    const formControl = input.parentElement;
-    const small = formControl.querySelector("small");
+  const formControl = input.parentElement;
+  const small = formControl.querySelector("small");
 }
